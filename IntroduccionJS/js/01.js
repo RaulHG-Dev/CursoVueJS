d7023978f9f36237d8bdffe4f6cd69418860b26c
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2023-12-31 11:37:12 -0600

    Video 08. Variables con let y sus caracteristicas

---
+++ IntroduccionJS/js/01.js
@@ -0,0 +1,14 @@
+// Variables con let
+let cliente = "Juan";
+let precioDescuento = 220;
+
+// Se puede reasignar
+cliente = 20;
+cliente = true;
+
+// pueden iniciar sin un valor
+let precio
+precio = 300;
+
+console.log(cliente)
+console.log(precio)
