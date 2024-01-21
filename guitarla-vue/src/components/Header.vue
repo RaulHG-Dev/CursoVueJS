bd6dc1715c9a3b5673e058c556822e030a685807
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-01-21 10:50:06 -0600

    Video 58. Mostrando el Contenido del carrito

--- guitarla-vue/src/components/Header.vue
+++ guitarla-vue/src/components/Header.vue
@@ -38,13 +38,19 @@
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
+                                        <tr
+                                            v-for="producto in carrito"
+                                        >
                                             <td>
-                                                <img class="img-fluid" src="/img/guitarra_02.jpg" alt="imagen guitarra">
+                                                <img 
+                                                    class="img-fluid" 
+                                                    :src="'/img/' + producto.imagen + '.jpg'" 
+                                                    :alt="'Imagen guitarra ' + producto.nombre"
+                                                />
                                             </td>
-                                            <td>SRV</td>
+                                            <td>{{ producto.nombre }}</td>
                                             <td class="fw-bold">
-                                                    $299
+                                                    ${{ producto.precio }}
                                             </td>
                                             <td class="flex align-items-start gap-4">
                                                 <button
@@ -53,7 +59,7 @@
                                                 >
                                                     -
                                                 </button>
-                                                    1
+                                                    {{ producto.cantidad }}
                                                 <button
                                                     type="button"
                                                     class="btn btn-dark"
