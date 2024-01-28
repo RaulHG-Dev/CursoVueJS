feb462a656da4b78bbee9154f90e332d50cec640
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-01-28 10:20:59 -0600

    Video 66. Solución Reto 01. Vaciar Carrito

--- guitarla-vue/src/App.vue
+++ guitarla-vue/src/App.vue
@@ -46,6 +46,10 @@
     const eliminarProducto = (id) => {
         carrito.value = carrito.value.filter(producto => producto.id !== id)
     }
+
+    const vaciarCarrito = () => {
+        carrito.value = []
+    }
 </script>
 
 <template>
@@ -56,6 +60,7 @@
         @incrementar-cantidad="incrementarCantidad"
         @agregar-carrito="agregarCarrito"
         @eliminar-producto="eliminarProducto"
+        @vaciar-carrito="vaciarCarrito"
     />
     <main class="container-xl mt-5">
         <h2 class="text-center">Nuestra Colección</h2>
--- guitarla-vue/src/components/Header.vue
+++ guitarla-vue/src/components/Header.vue
@@ -12,7 +12,7 @@
         }
     })
 
-    defineEmits(['incrementar-cantidad', 'decrementar-cantidad', 'agregar-carrito', 'eliminar-producto']);
+    defineEmits(['incrementar-cantidad', 'decrementar-cantidad', 'agregar-carrito', 'eliminar-producto', 'vaciar-carrito']);
 
     const totalPagar = computed(() => {
         return props.carrito.reduce(
@@ -98,7 +98,8 @@
                                 </table>
     
                                 <p class="text-end">Total pagar: <span class="fw-bold">${{ totalPagar }}</span></p>
-                                <button class="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
+
+                                <button class="btn btn-dark w-100 mt-3 p-2" @click="$emit('vaciar-carrito')">Vaciar Carrito</button>
                             </div>
                         </div>
                     </div>
