862edac61ea69584893185271261596087c28b99
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-02-04 10:54:34 -0600

    Video 77. Como leer datos ingresados a los inputs en Vue.js

--- admin-pacientes/src/components/Formulario.vue
+++ admin-pacientes/src/components/Formulario.vue
@@ -1,5 +1,9 @@
 <script setup>
+    import { ref } from 'vue';
 
+    const nombre = ref('')
+
+    const leerNombre = (e) => nombre.value = e.target.value
 </script>
 
 <template>
@@ -13,7 +17,14 @@
         <form class="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
             <div class="mb-5">
                 <label for="mascota" class="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
-                <input type="text" id="mascota" placeholder="Nombre de la mascota" class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md">
+                <input 
+                    type="text" 
+                    id="mascota" 
+                    placeholder="Nombre de la mascota" 
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    :value="nombre"
+                    @input="leerNombre"
+                />
             </div>
             <div class="mb-5">
                 <label for="propietario" class="block text-gray-700 uppercase font-bold">Nombre Propietario</label>
