bb5b41d114098d4c6849ef05dfce5172fc5de844
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-02-04 10:45:44 -0600

    Video 76. Finalizando el Formulario

--- admin-pacientes/src/components/Formulario.vue
+++ admin-pacientes/src/components/Formulario.vue
@@ -15,6 +15,23 @@
                 <label for="mascota" class="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
                 <input type="text" id="mascota" placeholder="Nombre de la mascota" class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md">
             </div>
+            <div class="mb-5">
+                <label for="propietario" class="block text-gray-700 uppercase font-bold">Nombre Propietario</label>
+                <input type="text" id="propietario" placeholder="Nombre de propietario" class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md">
+            </div>
+            <div class="mb-5">
+                <label for="email" class="block text-gray-700 uppercase font-bold">Email</label>
+                <input type="email" id="email" placeholder="Email del propietario" class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md">
+            </div>
+            <div class="mb-5">
+                <label for="alta" class="block text-gray-700 uppercase font-bold">Alta</label>
+                <input type="date" id="alta" class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md">
+            </div>
+            <div class="mb-5">
+                <label for="sintomas" class="block text-gray-700 uppercase font-bold">Síntomas</label>
+                <textarea placeholder="Describe los síntomas" id="sintomas" class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md h-40"></textarea>
+            </div>
+            <input type="submit" class="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors" value="Registrar Paciente">
         </form>
     </div>
 </template>
