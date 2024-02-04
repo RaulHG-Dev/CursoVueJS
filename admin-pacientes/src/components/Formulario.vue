d6953fdb5f36e30fcc971cddf8fc693c12ade309
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-02-04 11:27:48 -0600

    Video 83. Creando un State de Alertas para mensajes de error

--- admin-pacientes/src/components/Formulario.vue
+++ admin-pacientes/src/components/Formulario.vue
@@ -1,5 +1,11 @@
 <script setup>
     import { reactive } from 'vue';
+    import Alerta from './Alerta.vue';
+
+    const alerta = reactive({
+        tipo: '',
+        mensaje: ''
+    })
 
     const paciente = reactive({
         nombre: '',
@@ -12,6 +18,8 @@
     // const leerNombre = (e) => nombre.value = e.target.value
     const validar = () => {
         if(Object.values(paciente).includes('')) {
+            alerta.mensaje = 'Todos los campos son obligatorios';
+            alerta.tipo = 'error';
             return;
         }
     }
