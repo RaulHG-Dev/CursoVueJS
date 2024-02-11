40a28c9e4b3071fb3d18c173f7591342c7c6ca31
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-02-11 11:31:16 -0600

    Video 94. Generando un ID único para cada paciente

--- admin-pacientes/src/App.vue
+++ admin-pacientes/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
   import { ref, reactive } from 'vue';
+  import {uid} from 'uid';
   import Header from './components/Header.vue'
   import Formulario from './components/Formulario.vue'
   import Paciente from './components/Paciente.vue'
 
   const pacientes = ref([]);
   const paciente = reactive({
+    id: null,
     nombre: '',
     propietario: '',
     email: '',
@@ -15,7 +17,8 @@
 
   const guardarPaciente = () => {
     pacientes.value.push({
-      ...paciente
+      ...paciente,
+      id: uid()
     })
     // Reiniciar el objeto
     // paciente.nombre = ''
