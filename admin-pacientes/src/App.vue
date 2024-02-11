5bcf90449b4a6197914e79dc87b4202b07d54b14
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-02-11 10:28:05 -0600

    Video 86. Primeros pasos con el listado de pacientes

--- admin-pacientes/src/App.vue
+++ admin-pacientes/src/App.vue
@@ -1,14 +1,24 @@
 <script setup>
+  import { ref } from 'vue';
   import Header from './components/Header.vue'
   import Formulario from './components/Formulario.vue'
+
+  const pacientes = ref([]);
 </script>
 
 <template>
   <div class="container mx-auto mt-20">
-    <Header/>
+    <Header />
 
     <div class="mt-12 md:flex">
       <Formulario/>
+
+      <div class="md:w-1/2 md:h-screen overflow-y-scroll">
+        <h3 class="font-black text-3xl text-center">Administra tus Pacientes</h3>
+
+        <div v-if="pacientes.length > 0"></div>
+        <p v-else class="mt-20 text-2xl text-center">No Hay Pacientes</p>
+      </div>
     </div>
   </div>
 </template>
