d4faf24e6793b74370057980b3326cbb50cb180b
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-02-18 11:53:00 -0600

    Video 100. Solución Reto 02

--- admin-pacientes/src/App.vue
+++ admin-pacientes/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { ref, reactive } from 'vue';
+  import { ref, reactive, watch, onMounted } from 'vue';
   import {uid} from 'uid';
   import Header from './components/Header.vue'
   import Formulario from './components/Formulario.vue'
@@ -13,6 +13,23 @@
     email: '',
     alta: '',
     sintomas: ''
+  })
+
+  watch(pacientes, () => {
+    guardarLocalStorage();
+  }, {
+    deep: true
+  });
+
+  const guardarLocalStorage = () => {
+    localStorage.setItem('pacientes', JSON.stringify(pacientes.value));
+  }
+
+  onMounted(() => {
+    const pacienteStorage = localStorage.getItem('pacientes');
+    if(pacienteStorage) {
+      pacientes.value = JSON.parse(pacienteStorage);
+    }
   })
 
   const guardarPaciente = () => {
