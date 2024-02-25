15350f67f8181ee64433f04e68baf9ec208d1f58
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-02-25 09:21:50 -0600

    Video 106. Finalizando el Formulario de Presupuesto

--- admin-gastos/src/components/Presupuesto.vue
+++ admin-gastos/src/components/Presupuesto.vue
@@ -3,13 +3,16 @@
 </script>
 
 <template>
-    <form>
+    <form
+     class="presupuesto"
+    >
         <div class="campo">
             <label for="nuevo-presupuesto">Definir Presupuesto</label>
             <input 
                 id="nuevo-presupuesto"
                 class="nuevo-presupuesto"
                 placeholder="Añade tu presupuesto"
+                type="number"
             />
         </div>
         <input type="submit" value="Definir Presupuesto"/>
@@ -17,5 +20,40 @@
 </template>
 
 <style scoped>
-
+    .presupuesto {
+        width: 100%;
+    }
+    .campo {
+        display: grid;
+    }
+    .presupuesto label {
+        font-size: 2.2rem;
+        text-align: center;
+        color: var(--azul);
+    }
+    .presupuesto input[type="number"] {
+        background-color: var(--gris-claro);
+        border-radius: 1rem;
+        padding: 1rem;
+        border: none;
+        font-size: 2rem;
+        text-align: center;
+        margin-top: 1rem;
+    }
+    .presupuesto input[type="submit"] {
+        background-color: var(--azul);
+        border: none;
+        padding: 1rem;
+        text-align: center;
+        font-size: 2rem;
+        margin-top: 2rem;
+        color: var(--blanco);
+        font-weight: 900;
+        width: 100%;
+        transition: background-color 300ms ease;
+    }
+    .presupuesto input[type="submit"]:hover {
+        background-color: #1048a4;
+        cursor: pointer;
+    }
 </style>
