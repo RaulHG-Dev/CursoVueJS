72bf577b2a7d6c6dfa1de27b7ed56eaa82e7bb8e
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-03-03 11:01:49 -0600

    Video 108. Mostrando una alerta de error si el presupuesto es inválido

--- admin-gastos/src/components/Presupuesto.vue
+++ admin-gastos/src/components/Presupuesto.vue
@@ -1,11 +1,16 @@
 <script setup>
     import { ref } from 'vue';
+    import Alerta from './Alerta.vue';
 
     const presupuesto = ref(0)
+    const error = ref('')
 
     const definirPresupuesto = () => {
         if(presupuesto.value <= 0) {
-
+            error.value = 'Presupuesto no válido'
+            setTimeout(() => {
+                error.value = '';
+            }, 3000);
         }
     }
 </script>
@@ -15,6 +20,9 @@
      class="presupuesto"
      @submit.prevent="definirPresupuesto"
     >
+        <Alerta v-if="error">
+            {{ error }}
+        </Alerta>
         <div class="campo">
             <label for="nuevo-presupuesto">Definir Presupuesto</label>
             <input 
