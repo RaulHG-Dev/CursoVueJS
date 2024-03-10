af91ffe44cd2644070cdcc30ee5f1735ec425a80
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-03-10 10:51:34 -0600

    Video 114. Añadiendo un botón para Nuevos Gastos

--- admin-gastos/src/App.vue
+++ admin-gastos/src/App.vue
@@ -3,6 +3,8 @@
     import Presupuesto from './components/Presupuesto.vue';
     import ControlPresupuesto from './components/ControlPresupuesto.vue';
 
+    import iconoNuevoGasto from './assets/img/nuevo-gasto.svg'
+
     const presupuesto = ref(0);
     const disponible = ref(0)
 
@@ -28,6 +30,14 @@
                 />
             </div>
         </header>
+        <main v-if="presupuesto > 0">
+            <div class="crear-gasto">
+                <img 
+                    :src="iconoNuevoGasto" 
+                    alt="icono nuevo gasto"
+                />
+            </div>
+        </main>
     </div>
 </template>
 
@@ -86,5 +96,14 @@
         background-color: var(--blanco);
         border-radius: 1.2rem;
         padding: 5rem;
+    }
+    .crear-gasto {
+        position: fixed;
+        bottom: 5rem;
+        right: 5rem;
+    }
+    .crear-gasto img {
+        width: 5rem;
+        cursor: pointer;
     }
 </style>
