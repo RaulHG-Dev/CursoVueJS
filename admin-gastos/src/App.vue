86ecbbeefc6ceb0d9b6febb58a5b758760512e16
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-03-24 12:49:01 -0600

    Video 130. Fijar el Modal si tenemos muchos gastos

--- admin-gastos/src/App.vue
+++ admin-gastos/src/App.vue
@@ -62,7 +62,9 @@
 </script>
 
 <template>
-    <div>
+    <div
+        :class="{fijar: modal.mostrar}"
+    >
         <header>
             <h1>Planificador de Gastos</h1>
             <div class="contenedor-header contenedor sombra">
@@ -139,6 +141,10 @@
     }
     h2 {
         font-size: 3rem;
+    }
+    .fijar {
+        overflow: hidden;
+        height: 100vh;
     }
     header {
         background-color: var(--azul);
