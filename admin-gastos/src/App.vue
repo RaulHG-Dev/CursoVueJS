b336abb650bdf14ee913dc93fbba906954691567
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-03-24 13:00:50 -0600

    Video 132. Solución Reto 03

--- admin-gastos/src/App.vue
+++ admin-gastos/src/App.vue
@@ -28,6 +28,8 @@
     watch(gastos, () => {
         const totalGastado = gastos.value.reduce((total, gasto) => gasto.cantidad + total, 0)
         gastado.value = totalGastado
+
+        disponible.value = presupuesto.value - totalGastado;
     }, {
         deep: true
     })
