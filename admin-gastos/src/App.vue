6bce416cd5808bbc2305bb3a29fd6457e5669fdb
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-04-07 11:12:33 -0600

    Video 141. Creando un Componente para filtrar gastos por categoría

--- admin-gastos/src/App.vue
+++ admin-gastos/src/App.vue
@@ -2,6 +2,7 @@
     import { ref, reactive, watch } from 'vue';
     import Presupuesto from './components/Presupuesto.vue';
     import ControlPresupuesto from './components/ControlPresupuesto.vue';
+    import Filtros from './components/Filtros.vue';
     import iconoNuevoGasto from './assets/img/nuevo-gasto.svg';
     import Gasto from './components/Gasto.vue';
     import { generarId } from './helpers';
@@ -14,6 +15,7 @@
     const presupuesto = ref(0);
     const disponible = ref(0);
     const gastado = ref(0);
+    const filtro = ref('');
 
     const gasto = reactive({
         nombre: '',
@@ -120,6 +122,9 @@
             </div>
         </header>
         <main v-if="presupuesto > 0">
+            <Filtros
+                v-model:filtro="filtro"
+            />
             <div class="listado-gastos contenedor">
                 <h2>{{ gastos.length > 0 ? 'Gastos' : 'No hay Gastos' }}</h2>
 
