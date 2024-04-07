bd16d0bd97a4d07a2059acfe6bc115ffbeaaa6c3
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-04-07 11:36:27 -0600

    Video 144. Solución Reto 05

--- admin-gastos/src/App.vue
+++ admin-gastos/src/App.vue
@@ -30,8 +30,9 @@
     watch(gastos, () => {
         const totalGastado = gastos.value.reduce((total, gasto) => gasto.cantidad + total, 0)
         gastado.value = totalGastado
-
         disponible.value = presupuesto.value - totalGastado;
+
+        localStorage.setItem('gastos', JSON.stringify(gastos.value));
     }, {
         deep: true
     })
@@ -53,6 +54,11 @@
         if(presupuestoStorage) {
             presupuesto.value = Number(presupuestoStorage);
             disponible.value = Number(presupuestoStorage);
+        }
+
+        const gastosStorage = localStorage.getItem('gastos');
+        if (gastosStorage) {
+            gastos.value = JSON.parse(localStorage.getItem('gastos'));
         }
     });
 
