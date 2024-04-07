38a1872b5bb552a03bdf2a3c2b341f13598b3eb4
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-04-07 12:43:28 -0600

    Video 147. Mostrando un Procentaje de gastos

--- admin-gastos/src/components/ControlPresupuesto.vue
+++ admin-gastos/src/components/ControlPresupuesto.vue
@@ -1,4 +1,5 @@
 <script setup>
+    import { computed } from 'vue';
     import CircleProgress from 'vue3-circle-progress';
     import "vue3-circle-progress/dist/circle-progress.css";
     import { formatearCantidad } from '../helpers';
@@ -19,13 +20,18 @@
             required: true
         }
     })
+
+    const porcentaje = computed(() => {
+        return parseInt(((props.presupuesto - props.disponible) / props.presupuesto) * 100);
+    });
 </script>
 
 <template>
     <div class="dos-columnas">
-        <div class="contendor-grafico">
+        <div class="contenedor-grafico">
+            <p class="porcentaje">{{ porcentaje }}%</p>
             <CircleProgress
-                :percent="20"
+                :percent="porcentaje"
                 :size="250"
                 :border-width="30"
                 :border-bg-width="30"
@@ -56,6 +62,21 @@
 </template>
 
 <style scoped>
+    .contenedor-grafico {
+        position: relative;
+    }
+    .porcentaje {
+        position: absolute;
+        margin: auto;
+        top: calc(50% - 1.5rem);
+        left: 0;
+        right: 0;
+        text-align: center;
+        z-index: 100;
+        font-size: 3rem;
+        font-weight: 900;
+        color: var(--gris-oscuro);
+    }
     .dos-columnas {
         display: flex;
         flex-direction: column;
