175249fa331e71b5d87a80f32a9e28771a2948b3
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-04-07 10:52:48 -0600

    Video 139. Creando el botón para eliminar Gastos

--- admin-gastos/src/components/Modal.vue
+++ admin-gastos/src/components/Modal.vue
@@ -144,6 +144,13 @@
                     :value="[isEditing ? 'Guardar Cambios' : 'Añadir Gasto']"
                 />
             </form>
+            <button
+                type="button"
+                class="btn-eliminar"
+                v-if="isEditing"
+            >
+                Eliminar Gasto
+            </button>
         </div>
     </div>
 </template>
@@ -210,6 +217,17 @@
         background-color: var(--azul);
         color: var(--blanco);
         font-weight: 700;
+        cursor: pointer;
+    }
+    .btn-eliminar {
+        border: none;
+        padding: 1rem;
+        width: 100%;
+        background-color: #ef4444;
+        font-weight: 700;
+        font-size: 1.2rem;
+        color: var(--blanco);
+        margin-top: 10rem;
         cursor: pointer;
     }
 </style>
