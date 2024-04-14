f331756c4e3307abf0606704a1da673adf8aec89
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-04-14 14:24:13 -0600

    Video 151. Comenzando con Nuestra aplicación

--- cripto-vue/src/App.vue
+++ cripto-vue/src/App.vue
@@ -1,7 +1,25 @@
 <script setup>
-
+  import {ref} from 'vue';
+  const monedas = ref([
+    { codigo: 'USD', texto: 'Dolar de Estados Unidos'},
+    { codigo: 'MXN', texto: 'Peso Mexicano'},
+    { codigo: 'EUR', texto: 'Euro'},
+    { codigo: 'GBP', texto: 'Libra Esterlina'},
+  ]);
 </script>
 
 <template>
+  <div class="contenedor">
+    <h1 class="titulo">Cotizador de <span>Criptomonedas</span></h1>
 
+    <div class="contenido">
+      <form class="formulario">
+        <label for="moneda">Moneda:</label>
+        <select id="moneda">
+          <option value="">-- Selecciona --</option>
+          <option v-for="moneda in monedas" :value="moneda.codigo">{{ moneda.texto }}</option>
+        </select>
+      </form>
+    </div>
+  </div>
 </template>
