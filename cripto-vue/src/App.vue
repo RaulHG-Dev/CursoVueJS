7478a782c5958cbb8930c057865fda343b01433c
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-04-21 13:35:56 -0600

    Video 157. Obtener el resultado de la cotización dinamicamente

--- cripto-vue/src/App.vue
+++ cripto-vue/src/App.vue
@@ -16,6 +16,8 @@
     criptomoneda: ''
   });
 
+  const cotizacion = ref({});
+
   onMounted(() => {
     const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD';
     fetch(url)
@@ -35,6 +37,10 @@
   const obtenerCotizacion = async () => {
     const { moneda, criptomoneda } = cotizar;
     const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+
+    const respuesta = await fetch(url);
+    const data = await respuesta.json();
+    cotizacion.value = data.DISPLAY[criptomoneda][moneda];
   }
 </script>
 
