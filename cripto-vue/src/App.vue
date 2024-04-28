200474d6938d9682fa5c6c0f8e98bf8b60f2bdd2
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-04-28 10:15:16 -0600

    Video 162. Moviendo nuestro código a un Composable

--- cripto-vue/src/App.vue
+++ cripto-vue/src/App.vue
@@ -1,19 +1,12 @@
 <script setup>
-  import {ref, reactive, onMounted, computed} from 'vue';
+  import {ref, reactive} from 'vue';
   import Alerta from './components/Alerta.vue';
   import Spinner from './components/Spinner.vue';
   import useCripto from './composables/useCripto';
 
-  const {cotizarMoneda} = useCripto();
-  cotizarMoneda();
+  const {monedas, criptomonedas, cargando, cotizacion, obtenerCotizacion, mostrarResultado} = useCripto();
 
-  const monedas = ref([
-    { codigo: 'USD', texto: 'Dolar de Estados Unidos'},
-    { codigo: 'MXN', texto: 'Peso Mexicano'},
-    { codigo: 'EUR', texto: 'Euro'},
-    { codigo: 'GBP', texto: 'Libra Esterlina'},
-  ]);
-  const criptomonedas = ref([]);
+  
   const error = ref('');
 
   const cotizar = reactive({
@@ -21,40 +14,17 @@
     criptomoneda: ''
   });
 
-  const cotizacion = ref({});
-  const cargando = ref(false);
-
-  onMounted(() => {
-    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD';
-    fetch(url)
-      .then(respuesta => respuesta.json())
-      .then(({Data}) =>  criptomonedas.value = Data);
-  })
-
   const cotizarCripto = () => {
     if(Object.values(cotizar).includes('')) {
       error.value = 'Todos los campos son obligatorios';
       return;
     }
     error.value = '';
-    obtenerCotizacion();
+    obtenerCotizacion(cotizar);
   }
 
-  const obtenerCotizacion = async () => {
-    cargando.value = true;
-    cotizacion.value = {};
-    const { moneda, criptomoneda } = cotizar;
-    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
 
-    const respuesta = await fetch(url);
-    const data = await respuesta.json();
-    cotizacion.value = data.DISPLAY[criptomoneda][moneda];
-    cargando.value = false;
-  }
 
-  const mostrarResultado = computed(() => {
-    return Object.values(cotizacion.value).length > 0;
-  });
 </script>
 
 <template>
