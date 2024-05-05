501a93d589e00ed5d385a56cce34eb5ef0955bf4
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-05-05 10:37:37 -0600

    Video 167. Colocando en el State los datos ingresados

--- clima-vue/src/components/Formulario.vue
+++ clima-vue/src/components/Formulario.vue
@@ -1,4 +1,11 @@
 <script setup>
+    import { reactive } from 'vue';
+
+    const busqueda = reactive({
+        ciudad: '',
+        pais: ''
+    })
+
     const paises = [
         { codigo: 'US', nombre: 'Estados Unidos' },
         { codigo: 'MX', nombre: 'México' },
@@ -20,12 +27,16 @@
                 type="text"
                 id="ciudad"
                 placeholder="Ciudad"
+                v-model="busqueda.ciudad"
             >
         </div>
         
         <div class="campo">
             <label for="pais">País</label>
-            <select id="pais">
+            <select 
+                id="pais"
+                v-model="busqueda.pais"
+            >
                 <option value="">-- Seleccione un país --</option>
                 <option v-for="pais in paises" :value="pais.codigo">{{ pais.nombre }}</option>
             </select>
