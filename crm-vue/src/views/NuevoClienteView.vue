852b21f68d00e9e0867ebf556470e5ecc3d3d415
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-05-26 10:54:41 -0600

    Video 187. Creando la Vista y Ruta para Nuevos Clientes

--- crm-vue/src/views/NuevoClienteView.vue
+++ crm-vue/src/views/NuevoClienteView.vue
@@ -5,10 +5,10 @@
 <template>
     <div>
         <div>
-            <RouterLink :to="{ name: 'agregar-cliente' }">
-                Agregar Cliente
+            <RouterLink :to="{ name: 'inicio' }">
+                Volver
             </RouterLink>
         </div>
-        <h1 class="text-4xl font-extrabold text-slate-500">Clientes</h1>
+        <h1 class="text-4xl font-extrabold text-slate-500">Agregar Cliente</h1>
     </div>
 </template>
