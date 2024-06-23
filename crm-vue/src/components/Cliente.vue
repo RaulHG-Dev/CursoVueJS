958b2390af60b8542f2094d2b5d46d3da663438b
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-06-23 09:48:56 -0600

    Video 212. Detectando el Estado del Cliente

--- crm-vue/src/components/Cliente.vue
+++ crm-vue/src/components/Cliente.vue
@@ -11,6 +11,10 @@
     const nombreCliente = computed(() => {
         return props.cliente.nombre + ' ' + props.cliente.apellido;
     });
+
+    const estadoCliente = computed(() => {
+        return props.cliente.estado;
+    });
 </script>
 
 <template>
@@ -24,7 +28,12 @@
             <p class="text-gray-600">{{ cliente.puesto }}</p>
         </td>
         <td class="whitespace-nowrap px-3 py-4 text-sm">
-
+            <button
+                class="inline-text rounded-full px-2 text-xs font-semibold leading-5"
+                :class="[estadoCliente ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800']"
+            >
+                {{ estadoCliente ? 'Activo' : 'Inactivo' }}
+            </button>
         </td>
         <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500 ">
             <RouterLink 
