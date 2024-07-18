7fb383e2d20b2b4c26a4e4ac32586974731ce167
Raul Hidalgo <rhidalgog@izzi.mx>
2024-07-18 13:44:37 -0600

    Video 240. Comunicación entre Stores para Mostrar y ocultar el Modal

--- bebidas-vue/src/stores/bebidas.js
+++ bebidas-vue/src/stores/bebidas.js
@@ -1,8 +1,11 @@
 import { ref, reactive, onMounted } from "vue";
 import { defineStore } from "pinia";
 import APIService from "@/services/APIService";
+import { useModalStore } from "./modal";
 
 export const useBebidasStore = defineStore('bebidas', () => {
+    const modal = useModalStore();
+
     const categorias = ref([]);
     const busqueda = reactive({
         nombre: '',
@@ -23,6 +26,8 @@ export const useBebidasStore = defineStore('bebidas', () => {
     async function seleccionarBebida(id) {
         const {data: {drinks}} = await APIService.buscarReceta(id);
         console.log(drinks);
+
+        modal.handleClickModal();
     }
 
     return {
