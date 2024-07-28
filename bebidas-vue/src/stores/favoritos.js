d2ec86a856240267af867360b3642711aa86c8f9
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-07-28 10:16:04 -0600

    Video 251. Mostrando Texto cuando no hay favoritos

--- bebidas-vue/src/stores/favoritos.js
+++ bebidas-vue/src/stores/favoritos.js
@@ -1,4 +1,4 @@
-import { ref, watch, onMounted } from "vue";
+import { ref, watch, onMounted, computed } from "vue";
 import { defineStore } from "pinia";
 import { useBebidasStore } from "./bebidas";
 import { useModalStore } from "./modal";
@@ -45,9 +45,12 @@ export const useFavoritosStore = defineStore('favoritos', () => {
         modal.modal = false;
     }
 
+    const noFavoritos = computed(() => favoritos.value.length === 0);
+
     return {
         favoritos,
         handleClickFavorito,
-        existeFavorito
+        existeFavorito,
+        noFavoritos
     }
 });
