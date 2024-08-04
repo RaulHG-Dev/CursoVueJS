77fd10970b5e2a2540693d66304ef172c406ba65
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-08-04 11:35:32 -0600

    Video 252. Mostrando Texto cuando no hay recetas o el usuario no ha buscado

--- bebidas-vue/src/views/InicioView.vue
+++ bebidas-vue/src/views/InicioView.vue
@@ -7,8 +7,10 @@
 
 <template>
     <h1 class="text-6xl font-extrabold">Recetas</h1>
+    
+    <p v-if="bebidas.noRecetas" class="my-10 text-center text-2xl">No hay Resultados, utiliza el formulario para buscar recetas</p>
 
-    <div class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
+    <div v-else class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
         <Receta
             v-for="receta in bebidas.recetas"
             :receta="receta"
