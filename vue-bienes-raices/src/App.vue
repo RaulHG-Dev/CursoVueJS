678f43cfeaaea8cd3f006f8ea470363f8bf79b14
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-08-11 09:52:13 -0600

    Video 261. Componentes en Vuetify

--- vue-bienes-raices/src/App.vue
+++ vue-bienes-raices/src/App.vue
@@ -3,5 +3,26 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
+  <!-- <RouterView /> -->
+    <v-card
+      elevation="3"
+      max-width="1200"
+      class="mx-auto"
+    >
+      <v-layout>
+        <v-app-bar
+          color="blue-darken-1"
+        >
+          <template v-slot:append>
+            
+          </template>
+          <template v-slot:prepend>
+            
+          </template>
+        </v-app-bar>
+        <v-main>
+          <RouterView/>
+        </v-main>
+      </v-layout>
+    </v-card>
 </template>
