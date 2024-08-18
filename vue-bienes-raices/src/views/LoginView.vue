406cb430548c3c5e2a1d811669005344b026c7a5
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-08-18 10:52:36 -0600

    Video 268. Creando un Store de Autenticación

--- vue-bienes-raices/src/views/LoginView.vue
+++ vue-bienes-raices/src/views/LoginView.vue
@@ -1,23 +1,16 @@
 <script setup>
     import { useForm, useField } from 'vee-validate';
-    import { useFirebaseAuth } from 'vuefire'
-    import { signInWithEmailAndPassword } from 'firebase/auth';
     import { loginSchema as validationSchema } from '../validation/loginSquema'
+    import { useAuthStore } from '@/stores/auth';
 
     const {handleSubmit} = useForm({ validationSchema });
-    const auth = useFirebaseAuth();
+    const auth = useAuthStore();
 
     const email = useField('email');
     const password = useField('password');
 
     const submit = handleSubmit((values) => {
-        signInWithEmailAndPassword(auth, values.email, values.password)
-            .then((userCredential) => {
-                console.log(userCredential);
-            })
-            .catch((error) => {
-                console.log(error.code);
-            });
+        auth.login(values);
     });
     // console.log(email);
 </script>
